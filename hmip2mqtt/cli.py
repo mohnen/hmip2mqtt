0f35817b24ac72acb1cf4d38044e7f0246995efe
Markus Mohnen <markus.mohnen@gmail.com>
2021-03-05 20:30:23 +0100

    Some more

--- hmip2mqtt/cli.py
+++ hmip2mqtt/cli.py
@@ -1,3 +1,6 @@
+import homematicip
+from homematicip.home import Home
+
 from pathlib import Path
 
 import typer
@@ -8,7 +11,19 @@ appdirs = AppDirs("hmip2mqtt", "")
 
 @app.command("run")
 def run():
-  typer.echo('Hello General Kenobi')
+    config = homematicip.find_and_load_config_file()
+    if config is None:
+        typer.echo("No configuration found", err=True)
+        raise typer.Exit(code=1)
+    else:
+        home = Home()
+        home.set_auth_token(config.auth_token)
+        home.init(config.access_point)
+        home.get_current_state()
+        for group in [group if group.groupType=="META" for group in home.groups]:
+            typer.echo(group)
+            # if group.groupType=="META":
+            #     for device in group.devices:
 
 def main():
   app()
