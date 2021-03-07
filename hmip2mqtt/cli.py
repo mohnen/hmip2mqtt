c4866f948272f5a486f87f5f11bafcaa064d229f
Markus Mohnen <markus.mohnen@gmail.com>
2021-03-07 18:22:51 +0100

    Initial connect to home and mqtt

--- hmip2mqtt/cli.py
+++ hmip2mqtt/cli.py
@@ -1,13 +1,15 @@
+import json
+# from pathlib import Path
+
 import homematicip
 from homematicip.home import Home
-
-from pathlib import Path
+from paho.mqtt import client as mqtt_client
 
 import typer
-from appdirs import AppDirs
+# from appdirs import AppDirs
 
 app = typer.Typer()
-appdirs = AppDirs("hmip2mqtt", "")
+# appdirs = AppDirs("hmip2mqtt", "")
 
 @app.command("run")
 def run():
@@ -19,11 +21,35 @@ def run():
         home = Home()
         home.set_auth_token(config.auth_token)
         home.init(config.access_point)
+
+
+        broker = 'house.mohnen.net'
+        port = 1883
+        client_id = 'homematicip'
+
+        def on_connect(client, userdata, flags, rc):
+            if rc == 0:
+                print(f"Connected to {broker}:{port} as {client_id}")
+            else:
+                typer.echo(f"Failed to connect, return code {rc}", err=True)
+                raise typer.Exit(code=1)
+        client = mqtt_client.Client(client_id)
+        # client.username_pw_set(username, password)
+        
+        client.on_connect = on_connect
+        try:
+            client.connect(broker, port)
+        except:
+            typer.echo(f"Failed to connect to {broker}:{port}", err=True)
+            raise typer.Exit(code=1)
+        client.loop_start()
+
         home.get_current_state()
-        for group in [group if group.groupType=="META" for group in home.groups]:
-            typer.echo(group)
-            # if group.groupType=="META":
-            #     for device in group.devices:
+        for group in home.groups:
+            if group.groupType=="META":
+                typer.echo(group)
+                for device in group.devices:
+                    pass#typer.echo(json.dumps(device._rawJSONData))
 
 def main():
   app()
